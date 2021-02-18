5071d92a1a51818d656b06597ff1af16863bb306
Guilherme Assis <guilherme.assis@codificar.com.br>
2021-02-18 11:26:41 -0300

    postback billet

--- src/routes/postbackBillet.php
+++ src/routes/postbackBillet.php
@@ -4,5 +4,6 @@ Route::group(array('namespace' => 'Codificar\Finance\Http\Controllers'), functio
     //Sao duas rotas: uma eh a rota que possui controler, e que tem o ledgerid
     //A outra eh a rota que nomeia, para utilizar ela dentro do controller
     Route::post('/libs/finance/postback/{transactionid}', 'GatewayPostbackController@postbackBillet');
+    Route::get('/libs/finance/postback/{transactionid}', 'GatewayPostbackController@postbackBillet');
     Route::post('/libs/finance/postback')->name('GatewayPostbackBillet');
 });
