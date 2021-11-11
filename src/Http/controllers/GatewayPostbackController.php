adc18c211f6d6a3b34f3cf1b2c6caa5b600feda1
guilherme.assis <guilherme.assis@codificar.com.br>
2021-11-11 11:40:15 -0300

    fix postback

--- src/Http/controllers/GatewayPostbackController.php
+++ src/Http/controllers/GatewayPostbackController.php
@@ -12,47 +12,30 @@ use Finance, Settings;
 class GatewayPostbackController extends Controller
 {
     /**
-     * Recebe uma notificacao quando o status da transacao (boleto ou pix) e alterado
+     * Recebe uma notificacao quando o status da transacao (boleto ou pix) muda
      */
     public function postbackBillet($transactionid, Request $request)
     {
-        $transaction = Transaction::find($transactionid);
-
-        //se nao encontrou a transaction (ex: gateway juno que todos postbacks tem a mesma rota), tenta pegar ela pelos parametros enviados pelo gateway ($request)
-        if(!$transaction) {
-            $gateway = PaymentFactory::createGateway();
-            $billetVerify = $gateway->billetVerify($request, $transactionid);
-            if($billetVerify['transaction_id']) {
-                $transaction = Transaction::find($billetVerify['transaction_id']);
-            }
-        }
+        $gateway = PaymentFactory::createGateway();
+        $billetVerify = $gateway->billetVerify($request, $transactionid);
         
-        //se a transaction e referente a confirmacao de pagamento pix
-        if($transaction && isset($transaction->pix_copy_paste) && $transaction->pix_copy_paste) {
+        if($billetVerify['transaction_id']) {
+            $transaction = Transaction::find($billetVerify['transaction_id']);
+        } else {
+            $transaction = Transaction::find($transactionid);
+        }
+       
+        if ($transaction && $transaction->ledger_id && $billetVerify['success'] && $billetVerify['status'] == 'paid') {
+            //Check if the transaction status is not paid. If is paid, so we cant add a balance value for user again
+            //Check se a transaction esta com status diferente de "pago", para evitar pagar em duplicidade. 
             //Se a transaction ja esta com status pago, nao faz sentido adicionar um saldo para o usuario novamente
             if($transaction->status != "paid") {
-                $gateway = PaymentFactory::createGateway();
-                $res = $gateway->retrievePix($transaction->gateway_transaction_id);
-                if($res && $res['paid']) {
-                    $finance = Finance::createCustomEntry($transaction->ledger_id, Finance::SEPARATE_CREDIT, "Pagamento Pix", $transaction->gross_value, null, null);
-                    $transaction->status = 'paid';
-                    $transaction->save();
-                }
-            }
-        } else { //se o postback e referente a confirmacao de pagamento boleto
-            $gateway = PaymentFactory::createGateway();
-            $billetVerify = $gateway->billetVerify($request, $transactionid);
-        
-            if ($transaction && $transaction->ledger_id && $billetVerify['success'] && $billetVerify['status'] == 'paid') {
-                //Se a transaction ja esta com status pago, nao faz sentido adicionar um saldo para o usuario novamente
-                if($transaction->status != "paid") {
-                    $tax = Settings::findByKey('prepaid_tax_billet');
-                    $tax = $tax ? (float) $tax : 0;
-                    //Add balance for user
-                    $finance = Finance::createCustomEntry($transaction->ledger_id, Finance::SEPARATE_CREDIT, "Credito referente ao boleto pago", $transaction->gross_value - $tax, null, null);
-                    $transaction->status = 'paid';
-                    $transaction->save();
-                }
+                $tax = Settings::findByKey('prepaid_tax_billet');
+                $tax = $tax ? (float) $tax : 0;
+                //Add balance for user
+                $finance = Finance::createCustomEntry($transaction->ledger_id, Finance::SEPARATE_CREDIT, "Credito referente ao boleto pago", $transaction->gross_value - $tax, null, null);
+                $transaction->status = 'paid';
+                $transaction->save();
             }
         }
     }
