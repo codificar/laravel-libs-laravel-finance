807d45a5801e5382b80e91611695330af1709ce9
Guilherme Assis <guilherme.assis@codificar.com.br>
2021-11-11 15:35:29 +0000

    Update postbackPix.php

--- src/routes/postbackPix.php
+++ src/routes/postbackPix.php
@@ -2,7 +2,7 @@
 
 Route::group(array('namespace' => 'Codificar\Finance\Http\Controllers'), function () {
     Route::post('/libs/finance/postback/pix/{transactionid}', 'GatewayPostbackController@postbackPix');
-    Route::gets('/libs/finance/postback/pix/{transactionid}', 'GatewayPostbackController@postbackPix');
+    Route::get('/libs/finance/postback/pix/{transactionid}', 'GatewayPostbackController@postbackPix');
     
     Route::post('/libs/finance/postback/pix')->name('GatewayPostbackPix');
 });
