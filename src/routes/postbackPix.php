4837cbe831d4a3abf6a2ac62fb8514890f5f1c35
randler <randlersi@gmail.com>
2022-07-05 16:01:03 -0300

    correção finance

--- src/routes/postbackPix.php
+++ src/routes/postbackPix.php
@@ -4,6 +4,7 @@ Route::group(array('namespace' => 'Codificar\Finance\Http\Controllers'), functio
     Route::post('/libs/finance/postback/pix/{transactionid}', 'GatewayPostbackController@postbackPix');
     Route::get('/libs/finance/postback/pix/{transactionid}', 'GatewayPostbackController@postbackPix');
     
-    Route::post('/libs/finance/postback/pix/ipag', 'GatewayPostbackController@postbackPixIpag');
-    Route::post('/libs/finance/postback/pix')->name('GatewayPostbackPix');
+    Route::post('/libs/finance/postback/pix', 'GatewayPostbackController@postbackPix');
+    Route::get('/libs/finance/postback/pix', 'GatewayPostbackController@postbackPix');
+    //Route::post('/libs/finance/postback/pix')->name('GatewayPostbackPix');
 });
