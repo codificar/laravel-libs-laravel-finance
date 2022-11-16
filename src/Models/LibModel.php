6aece79827a917de8cf453e168a1a5d1cd696305
Luiz Antunes <luiz.antunes@codificar.com.br>
2022-11-16 19:32:05 +0000

    Revert "Merge branch 'fix.24905' into 'master'"
    This reverts merge request !10

--- src/Models/LibModel.php
+++ src/Models/LibModel.php
@@ -583,15 +583,11 @@ class LibModel extends Eloquent
 		}
 
 		if ($keyWord != '') {
-			if($type == 'user') {
-				$leders->where(DB::raw('CONCAT_WS(" ", $type.first_name, user.last_name)'), "LIKE", '%' . $keyWord . '%')
-				->orWhere('user.email', "LIKE", '%' . $keyWord . '%');
-			}else{
-				$leders->where(DB::raw('CONCAT_WS(" ", provider.first_name, provider.last_name)'), "LIKE", '%' . $keyWord . '%')
+			$leders->where(DB::raw('CONCAT_WS(" ", user.first_name, user.last_name)'), "LIKE", '%' . $keyWord . '%')
+				->orWhere(DB::raw('CONCAT_WS(" ", provider.first_name, provider.last_name)'), "LIKE", '%' . $keyWord . '%')
+				->orWhere('user.email', "LIKE", '%' . $keyWord . '%')
 				->orWhere('provider.email', "LIKE", '%' . $keyWord . '%');
-			}
 		}
-
 
 		$leders->groupBy('ledger.id');
 
