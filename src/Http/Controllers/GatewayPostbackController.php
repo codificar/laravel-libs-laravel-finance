fa7b0ae90ba7deea4156002999b9dc2166772de2
randler <randler.ferraz@codificar.com.br>
2022-11-28 16:53:23 -0300

    correção postback

--- src/Http/Controllers/GatewayPostbackController.php
+++ src/Http/Controllers/GatewayPostbackController.php
@@ -16,10 +16,10 @@ class GatewayPostbackController extends Controller
     /**
      * Recebe uma notificacao quando o status da transacao boleto muda
      */
-    public function postbackBillet($transactionid, Request $request)
+    public function postbackBillet($transactionId, Request $request)
     {
         \Log::notice('transactionid: ' 
-            . $transactionid 
+            . $transactionId 
             .  ' - postbackBillet: ' 
             .  json_decode($request)
         );
@@ -30,10 +30,10 @@ class GatewayPostbackController extends Controller
 
         
         $gateway = PaymentFactory::createGateway();
-        $billetVerify = $gateway->billetVerify($request, $transactionid);
+        $billetVerify = $gateway->billetVerify($request, $transactionId);
         
-        if($transactionid && is_numeric($transactionid)) {
-            $transaction = Transaction::find($transactionid);
+        if($transactionId && is_numeric($transactionId)) {
+            $transaction = Transaction::find($transactionId);
         } else {
             $transaction = Transaction::find($billetVerify['transaction_id']);
         }
@@ -59,7 +59,7 @@ class GatewayPostbackController extends Controller
     /**
      * Recebe uma notificacao quando o status da transacao pix e alterada
      */
-    public function postbackPix($transactionid, Request $request)
+    public function postbackPix($transactionId, Request $request)
     {
         if($request && $request->method() == 'GET') {
             return Response::json(["success" => true], 200);
@@ -70,7 +70,7 @@ class GatewayPostbackController extends Controller
         if($gatewayPix == 'ipag') {
             $this->postbackPixIpag($request);
         } else if($gatewayPix == 'juno'){
-            $this->postbackPixJuno($transactionid, $request);
+            $this->postbackPixJuno($transactionId, $request);
         }
     }
 
@@ -81,8 +81,9 @@ class GatewayPostbackController extends Controller
     {
         $request = $request->all();
 
+        
         if( isset($request['id']) && !empty($request['id'])) {
-            $transaction = Transaction::getTransactionByGatewayId($request['id']);
+            $transaction = \Transaction::getTransactionByGatewayId($request['id']);
             $isSuccesss = $request['attributes']['status']['code'] == '8';
             
             if ($transaction && $transaction->ledger_id && $transaction->pix_copy_paste && $isSuccesss) {
@@ -92,18 +93,38 @@ class GatewayPostbackController extends Controller
                     
                     // se a transacao e referente a uma request
                     if($transaction->request_id) {
-                        $request = Requests::find($transaction->request_id);
-                        $request->is_paid = 1;
-                        $request->save();
+                        $ride = \Requests::find($transaction->request_id);
+                        $ride->is_paid = 1;
+                        $ride->save();
 
                         //gera saldo para o motorista
-                        if ($request->confirmedProvider && $request->confirmedProvider->Ledger) {
-                            Finance::createRideCredit($request->confirmedProvider->Ledger->id, $transaction->provider_value * -1, $request->id);
+                        if ($ride->confirmedProvider && $ride->confirmedProvider->Ledger) {
+                            \Finance::createRideCredit($ride->confirmedProvider->Ledger->id, $transaction->provider_value * -1, $ride->id);
                         }
-                    } 
+                    }  
+                    // Atualiza os dados de transação via Pix
+                    else if($transaction->signature_id) {
+                        $signature = \Signature::find($transaction->signature_id);
+                        if($signature) {
+                            $plan = \Plan::find($signature->plan_id);
+                             // Define a data de expiração da assinatura
+                            $period = $plan->period;
+                            $period = $plan->period + \Settings::getDaysForSubscriptionRecurrency();
+                            $nextExpiration = \Carbon::now()->addDays($period);
+
+                            $signature->created_at = \Carbon::now();
+                            $signature->next_expiration = $nextExpiration;
+                            $signature->activity = 1;
+                            $signature->save();
+                        }
+                        //gera saldo para o motorista
+                        if ($signature->provider && $signature->provider->Ledger) {
+                            \Finance::createRideCredit($signature->provider->Ledger->id, $transaction->provider_value * -1, null);
+                        }
+                    }
                     // se a transacao e pre-pago (ou seja, nao e referente a uma request) entao adiciona saldo 
                     else {
-                        $finance = Finance::createCustomEntry($transaction->ledger_id, Finance::SEPARATE_CREDIT, "Pagamento Pix", $transaction->gross_value, null, null);
+                        $finance = \Finance::createCustomEntry($transaction->ledger_id, Finance::SEPARATE_CREDIT, "Pagamento Pix", $transaction->gross_value, null, null);
                     }
                     $transaction->status = 'paid';
                     $transaction->save();
@@ -113,17 +134,17 @@ class GatewayPostbackController extends Controller
                 }
             }
         } else {
-            Log::error($request);
-            Log::error('Pix IPag: Nao foi possivel identificar a transacao');
+            \Log::error($request);
+            \Log::error('Pix IPag: Nao foi possivel identificar a transacao');
         }
     }
     /**
      * Recebe uma notificacao quando o status da transacao pix do Juno e alterada
      */
-    private function postbackPixJuno($transactionid, Request $request)
+    private function postbackPixJuno($transactionId, Request $request)
     {
         $gateway = PaymentFactory::createPixGateway();
-        $retrievePix = $gateway->retrievePix($transactionid, $request);
+        $retrievePix = $gateway->retrievePix($transactionId, $request);
         
         $transaction = Transaction::find($retrievePix['transaction_id']);
         
