ebac4d7f855685a92c9a73a4818853e4766f0497
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:37:29 -0300

    - correção tradução es-py

--- src/tests/translate/LibFinanceLanguageSpanishTest.php
+++ src/tests/translate/LibFinanceLanguageSpanishTest.php
@@ -19,7 +19,7 @@ class LibFinanceLanguageSpanishTest extends TestCase
         
         $path = base_path() . '/vendor/codificar/finance/src/resources/lang';
         $directoryPt = "$path/pt-br/*.php";
-        $directoryEs = "$path/es/*.php";
+        $directoryEs = "$path/es-py/*.php";
         $filesPt = glob($directoryPt);
         $filesEs = glob($directoryEs);
 
